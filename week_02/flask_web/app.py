a2bdbe8761d46523528290494b78e2b4c342d928
Mike Izbicki <mike@izbicki.me>
2022-02-04 08:07:58 -0800

    lecture notes

--- week_02/flask_web/app.py
+++ week_02/flask_web/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hey, we have Flask in a Docker container!'
+    return 'hello world did it work?!'
 
 
 if __name__ == '__main__':
